96f6b02d909e606b1f094ee24beb48069a8dc835
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-10-12 17:01:56 +0200

    re-namespace formatter to be compatible with newer cucumbers (ffs, please stop changing the formatter api)

--- lib/generators/project/features/support/nagios.rb
+++ lib/generators/project/features/support/nagios.rb
@@ -1,42 +1,48 @@
-module Nagios
-  class NagiosFormatter < Cucumber::Ast::Visitor
-    def initialize(step_mother, io, options={})
-      super(step_mother)
-      @failed  = []
-      @passed  = []
-      @warning = []
-    end
+require 'cucumber/formatter/console'
 
-    def visit_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
-      super
-      case status
+module Cucumber
+  module Formatter
+    class Nagios
+
+      def initialize(step_mother, io, options={})
+        @failed  = []
+        @passed  = []
+        @warning = []
+        @io = io
+      end
+  
+      def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
+        case status
         when :passed
           @passed << step_match
         when :failed
-          @failed << step_match
+           @failed << step_match
         when :undefined
           @warning << step_match
+        end
       end
-    end
+  
+      def after_features(steps)
+        print_summary
+      end
+  
+      private
+      def print_summary
+        @total = @failed.size + @passed.size + @warning.size
+        message = ""
+        message += "Critical: #{@failed.size}, "
+        message += "Warning: #{@warning.size}, "
+        message += "#{@passed.size} okay"
+        # nagios performance data
+        message += " | passed=#{@passed.size}"
+        message += ", failed=#{@failed.size}"
+        message += ", nosteps=#{@warning.size}"
+        message += ", total=#{@total}\n"
 
-    def visit_steps(steps)
-      super
-      print_summary
-    end
+        @io.print(message)
+        @io.flush
+      end
 
-    private
-    def print_summary
-      @total = @failed.size + @passed.size + @warning.size
-      message = ""
-      message += "Critical: #{@failed.size}, "
-      message += "Warning: #{@warning.size}, "
-      message += "#{@passed.size} okay"
-      # nagios performance data
-      message += " | passed=#{@passed.size}"
-      message += ", failed=#{@failed.size}"
-      message += ", nosteps=#{@warning.size}"
-      message += ", total=#{@total}"
-      puts message
     end
   end
 end

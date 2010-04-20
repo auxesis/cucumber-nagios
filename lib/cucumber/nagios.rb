88aac3b66dc93ecd21002ce5f709848a143565c3
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-04-20 17:09:50 +1000

    handle missing libraries better

--- lib/cucumber/nagios.rb
+++ lib/cucumber/nagios.rb
@@ -2,7 +2,16 @@ module Cucumber
   module Nagios
   end
 end
-require 'cucumber/nagios/version'
-require 'cucumber/nagios/command'
-require 'net/ssh'
-require 'webrat'
+
+begin 
+  require 'cucumber/nagios/version'
+  require 'cucumber/nagios/command'
+  require 'net/ssh'
+  require 'webrat'
+  require 'mq'
+rescue LoadError => e
+  dep = e.message.split.last
+  puts "You don't appear to have #{dep} installed." 
+  puts "Perhaps run `gem bundle` or `gem install #{dep}`?"
+  exit 2
+end

c592f348c49bea657e497bfc004a440488b4a2ca
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-06-24 00:05:06 +1000

    cleaned up nagios output generation, cleaned up whitespace

--- lib/cucumber/formatter/nagios.rb
+++ lib/cucumber/formatter/nagios.rb
@@ -10,7 +10,7 @@ module Cucumber
         @warning = []
         @io = io
       end
-  
+
       def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
         record_result(status, :step_match => step_match)
       end
@@ -20,7 +20,7 @@ module Cucumber
       end
 
       def after_table_row(table_row)
-        unless @header_row 
+        unless @header_row
           record_result(table_row.status) if table_row.respond_to?(:status)
         end
         @header_row = false if @header_row
@@ -29,24 +29,22 @@ module Cucumber
       def after_features(steps)
         print_summary
       end
-  
+
       private
       def print_summary
         @total = @failed.size + @passed.size + @warning.size
-        message = ""
-        message += "Critical: #{@failed.size}, "
-        message += "Warning: #{@warning.size}, "
-        message += "#{@passed.size} okay"
-        # nagios performance data
-        message += " | passed=#{@passed.size}"
-        message += ", failed=#{@failed.size}"
-        message += ", nosteps=#{@warning.size}"
-        message += ", total=#{@total}\n"
+        @status = @failed.size > 0 && "CRITICAL" || @warning.size > 0 && "WARNING" || "OK"
+
+        service_output   = [ "CUCUMBER #{@status} - Critical: #{@failed.size}",
+                             "Warning: #{@warning.size}", "#{@passed.size} okay" ]
+        performance_data = [ "passed=#{@passed.size}", "failed=#{@failed.size}",
+                             "nosteps=#{@warning.size}", "total=#{@total}" ]
+        message = "#{service_output.join(', ')} | #{performance_data.join('; ')}\n"
 
         @io.print(message)
         @io.flush
       end
-      
+
       def record_result(status, opts={})
         step_match = opts[:step_match] || true
         case status

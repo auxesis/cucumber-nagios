9b6439e562104146ee3737448d5036d6f49a6a0a
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-12-30 18:35:11 +1100

    Cleared up whitespace. Issue updated bundler commands.

--- features/steps/creating_steps.rb
+++ features/steps/creating_steps.rb
@@ -7,17 +7,18 @@ end
 When /^I create a new project called "([^\"]*)"$/ do |project_name|
   @project_name = project_name
   FileUtils.rm_rf("/tmp/#{@project_name}")
-  
+
   silent_system("cd /tmp ; cucumber-nagios-gen project #{@project_name}").should be_true
 end
 
 When /^I freeze in dependencies$/ do
   @project_name.should_not be_nil
-  silent_system("cd /tmp/#{@project_name} ; gem bundle").should be_true
+  silent_system("cd /tmp/#{@project_name} ; bundle install").should be_true
 end
 
-Then /^my gems directory should be populated$/ do
+Then /^a Gemfile lock should be created$/ do
   @project_name.should_not be_nil
-  Dir.glob("/tmp/#{@project_name}/vendor/gems/*").size.should > 0
+  File.exists?("/tmp/#{@project_name}/Gemfile.lock").should be_true
 end
+
 

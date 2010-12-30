8867e869fdf313176418fab19749c378e2a34fa1
Lindsay Holmwood <lindsay@holmwood.id.au>
2010-12-30 13:30:03 +1100

    Load up Webrat into World. Put the bundled cucumber/nagios/steps on the load path

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,9 @@
+#!/usr/bin/env ruby
+
+$: << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+
 require 'cucumber/nagios/steps'
+
+World do
+  Webrat::Session.new(Webrat::MechanizeAdapter.new)
+end

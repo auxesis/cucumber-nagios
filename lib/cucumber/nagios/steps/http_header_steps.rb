298adcb36b8e949c0b235e938530019a8441ad50
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-01-01 12:40:45 +1100

    Style improvements:
     - Remove whitespace in block arguments
     - Parenthesize method arguments
     - Rename variables so they are more clear & concise

--- lib/cucumber/nagios/steps/http_header_steps.rb
+++ lib/cucumber/nagios/steps/http_header_steps.rb
@@ -1,21 +1,20 @@
-When /I fetch (.*)/ do | url |
-  visit url
-  @url_headers = response.header
+When /I fetch headers from "(.*)"/ do |url|
+  visit(url)
+  @headers = response.header
 end
- 
-Then /^the "(.*)" header should be "(.*)"$/ do | header_name, header_value|
-  @url_headers[header_name].should == header_value
+
+Then /^the "(.*)" header should be "(.*)"$/ do |name, value|
+  @headers[name].should == value
 end
- 
-Then /^the response should contain the "(.*)" header$/ do | header_name |
-  @url_headers.should have_key(header_name.downcase)
+
+Then /^the response should contain the "(.*)" header$/ do |name|
+  @headers.should have_key(name.downcase)
 end
- 
-### header contains checks
-Then /^the "(.*)" header should contain "(.*)"$/ do | header_name, header_contains |
-  @url_headers[header_name].should match header_contains
+
+Then /^the "(.*)" header should contain "(.*)"$/ do |name, expression|
+  @headers[name].should match(expression)
 end
- 
-Then /^the "(.*)" header should not contain "(.*)"$/ do | header_name, header_contains |
-  @url_headers[header_name].should_not match header_contains
+
+Then /^the "(.*)" header should not contain "(.*)"$/ do |name, expression|
+  @headers[name].should_not match(expression)
 end

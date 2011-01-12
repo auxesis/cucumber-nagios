b9e6c3c7833d950184478c4207190d6a39e58585
Lindsay Holmwood <lindsay@holmwood.id.au>
2011-01-12 12:37:25 +1100

    Refactor step to correctly check for multiline output

--- features/steps/using_steps.rb
+++ features/steps/using_steps.rb
@@ -35,6 +35,8 @@ Then /^"([^"]*)" in the "([^"]*)" project should not exist$/ do |file, project_n
 end
 
 Then /^the "([^"]*)" feature on "([^"]*)" should produce multiline output$/ do |feature, site|
-  `cd /tmp/#{@project_name} ; bin/cucumber-nagios features/#{site}/#{feature}.feature`
+  @output = `cd /tmp/#{@project_name} ; bin/cucumber-nagios features/#{site}/#{feature}.feature`
   $?.exitstatus.should == 0
+
+  @output.split("\n").size.should > 1
 end
